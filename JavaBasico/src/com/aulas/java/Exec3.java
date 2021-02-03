8a6453dc236d6b599bbf07fb227aae2126cbf2b0
Pedro Henrique <pedrohbc96@gmail.com>
2021-02-03 19:10:33 -0300

    Otimizando codigo da Questao 3 do Exercicio 3

--- JavaBasico/src/com/aulas/java/Exec3.java
+++ JavaBasico/src/com/aulas/java/Exec3.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class Exec3 {
 
-// Exercicio 3 Questao 3 do curso de Java Basico
 	
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
@@ -17,13 +16,8 @@ public class Exec3 {
 		
 		int idade = 2021 - anoNasc;
 		
-		boolean maiorIdade = idade >= 18 ? true:false;
-		
-		if(maiorIdade) {
-			System.out.println("Seu nome é: "+nome+", sua idade é: "+idade+" e você é maior de idade!");
-		}else {
-			System.out.println("Seu nome é: "+nome+", sua idade é: "+idade+" e você é menor de idade!");
-		}
+		System.out.println("Seu nome é: "+nome+", sua idade é: "+idade+
+		(idade>=18?" e você é maior de idade!" : " e você é menor de idade!"));
 	}
 
 }
