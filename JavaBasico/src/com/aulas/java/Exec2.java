56cb0ded0a466649c3aa1804900b3cae5ba95707
Pedro Henrique <pedrohbc96@gmail.com>
2021-02-02 22:04:49 -0300

    Adicionando Exercício 2 do Cursod e Java Básico

---
+++ JavaBasico/src/com/aulas/java/Exec2.java
@@ -0,0 +1,19 @@
+package com.aulas.java;
+
+import java.util.Scanner;
+
+public class Exec2 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Digite o primeiro nome do usuario: ");
+		String primeiroNome = scan.nextLine();
+		
+		System.out.println("Digite o segundo nome do usuario: ");
+		String segundoNome = scan.nextLine();
+		
+		System.out.println("Ola "+primeiroNome+" "+ segundoNome+", Bem vindo ao curso de Java. \nQualiti Innovate Learning.");
+	}
+
+}
