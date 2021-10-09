da8a803cdd50f83971f835aba337e6c04f0318bb
Pedro Henrique <pedrohbc96@gmail.com>
2021-10-09 14:00:38 -0300

    Programa inicial

---
+++ src/TiposDeVariaveis.java
@@ -0,0 +1,24 @@
+
+public class TiposDeVariaveis {
+
+	public static void main(String[] args) {
+		byte tipoByte = 123; //Entre -128 e 127
+		short tipoShort = 12345;//Entre -32.768 e 32.767
+		char tipoChar = 'a'; 
+		float tipoFloat = 2.1f;
+		double tipoDouble = 1.52;
+		int tipoInt = 12345;
+		long tipoLong = 1234567890l;
+		boolean tipoBooleano = true;
+		System.out.println("Valor do tipoByte = " + tipoByte);
+		System.out.println("Valor do tipoShort = " + tipoShort);
+		System.out.println("Valor do tipoChar = "  + tipoChar);
+		System.out.println("Valor do tipoFloat = " +tipoFloat);
+		System.out.println("Valor do tipoDouble = " +tipoDouble);
+		System.out.println("Valor do tipoInt = " +tipoInt);
+		System.out.println("Valor do tipoLong = " +tipoLong);
+		System.out.println("Valor do tipoBooleano = " +tipoBooleano);
+
+	}
+
+}
